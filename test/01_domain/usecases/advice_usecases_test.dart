7aeb3d873ab08e707c461885136d0d310e095368
h <H>
2022-11-26 16:51:11 +0300

    add Unit Test - Usecases

---
+++ test/01_domain/usecases/advice_usecases_test.dart
@@ -0,0 +1,93 @@
+import 'package:clean_architecture/0_data/repositories/advice_repo_impl.dart';
+import 'package:clean_architecture/1_domain/entities/advice_entity.dart';
+import 'package:clean_architecture/1_domain/failures/failures.dart';
+import 'package:clean_architecture/1_domain/usecases/advice_usecases.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'advice_usecases_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<AdviceRepoImpl>()])
+void main() {
+  group(
+    'AdviceUsecases',
+    () {
+      // ARRANGE
+      final MockAdviceRepoImpl mockAdviceRepoImpl = MockAdviceRepoImpl();
+      final adviceUsecaseUnderTest = AdviceUseCases(adviceRepoImpl: mockAdviceRepoImpl);
+      group(
+        'should return AdviceEntity',
+        () {
+          test(
+            'when AdviceRepoImpl returns a AdviceModel',
+            () async {
+              // ARRANGE
+              const AdviceEntity mockAdviceModel = AdviceEntity(advice: 'advice', id: 2);
+
+              when(mockAdviceRepoImpl.getAdviceFromDataSource()).thenAnswer(
+                (realInvocation) => Future.value(
+                  const Right(mockAdviceModel),
+                ),
+              );
+              // ACT
+              final result = await adviceUsecaseUnderTest.getAdvice();
+
+              // ASSERT
+              expect(result.isLeft(), false);
+              expect(result.isRight(), true);
+              expect(result, const Right<Failure, AdviceEntity>(mockAdviceModel));
+              verify(mockAdviceRepoImpl.getAdviceFromDataSource()).called(1);
+              verifyNoMoreInteractions(mockAdviceRepoImpl);
+            },
+          );
+        },
+      );
+
+      group('should return left with', () {
+        test(
+          'a ServerFailure',
+          () async {
+            // ARRANGE
+            when(mockAdviceRepoImpl.getAdviceFromDataSource()).thenAnswer(
+              (realInvocation) => Future.value(
+                Left(ServerFailure()),
+              ),
+            );
+            // ACT
+            final Either<Failure, AdviceEntity> result = await adviceUsecaseUnderTest.getAdvice();
+
+            // ASSERT
+            expect(result.isLeft(), true);
+            expect(result.isRight(), false);
+            expect(result, Left<Failure, AdviceEntity>(ServerFailure()));
+            verify(mockAdviceRepoImpl.getAdviceFromDataSource()).called(1);
+            verifyNoMoreInteractions(mockAdviceRepoImpl);
+          },
+        );
+
+        test(
+          'a GeneralFailure',
+          () async {
+            // ARRANGE
+            when(mockAdviceRepoImpl.getAdviceFromDataSource()).thenAnswer(
+              (realInvocation) => Future.value(
+                Left(GeneralFailure()),
+              ),
+            );
+            // ACT
+            final Either<Failure, AdviceEntity> result = await adviceUsecaseUnderTest.getAdvice();
+
+            // ASSERT
+            expect(result.isLeft(), true);
+            expect(result.isRight(), false);
+            expect(result, Left<Failure, AdviceEntity>(GeneralFailure()));
+            verify(mockAdviceRepoImpl.getAdviceFromDataSource()).called(1);
+            verifyNoMoreInteractions(mockAdviceRepoImpl);
+          },
+        );
+      });
+    },
+  );
+}
